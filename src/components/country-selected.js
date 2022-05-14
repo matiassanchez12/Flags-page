b0572d75916184dc9da0d1110224acb6b8ca018e
Matias <matias.sanchez.1897@hotmail.com>
2022-05-14 13:29:48 -0300

    handle errors in country page

--- src/components/country-selected.js
+++ src/components/country-selected.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const CountrySelectedStyled = styled.div`
   margin-top: 3em;
   padding-bottom: 3em;
-  h2{
+  h2 {
     font-size: 1.5em;
   }
-  p{
+  p {
     font-size: 1em;
   }
   img {
@@ -20,10 +20,10 @@ const CountrySelectedStyled = styled.div`
     grid-row-gap: 1em;
   }
   .border-item {
-    padding: .5em 2em;
+    padding: 0.5em 2em;
     border-radius: 5px;
     margin-right: 15px;
-    box-shadow: 0 0 5px rgba(0,0,0,.3);
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
     display: inline-flex;
     margin-bottom: 15px;
     background: var(--white);
@@ -32,7 +32,7 @@ const CountrySelectedStyled = styled.div`
     span {
       margin-right: 5px;
       &:after {
-        content: ',';
+        content: ",";
       }
       &:last-child {
         &:after {
@@ -57,8 +57,10 @@ const CountrySelectedStyled = styled.div`
   }
 `;
 
-function CountrySelected (props) {
+function CountrySelected(props) {
   const country = props.country;
+  if (!country) return <h2>No se encontraron datos</h2>;
+
   return (
     <CountrySelectedStyled>
       <img src={country.flag} alt="" />
@@ -66,34 +68,59 @@ function CountrySelected (props) {
         <h2>{country.name}</h2>
         <div className="grid">
           <div>
-            <p><strong>Native Name:</strong>{country.nativeName}</p>
-            <p><strong>Population:</strong>{country.population}</p>
-            <p><strong>Region:</strong>{country.region}</p>
-            <p><strong>Sub Region:</strong>{country.subRegion}</p>
-            <p><strong>Capital:</strong>{country.capital}</p>
+            <p>
+              <strong>Native Name:</strong>
+              {country.nativeName}
+            </p>
+            <p>
+              <strong>Population:</strong>
+              {country.population}
+            </p>
+            <p>
+              <strong>Region:</strong>
+              {country.region}
+            </p>
+            <p>
+              <strong>Sub Region:</strong>
+              {country.subRegion}
+            </p>
+            <p>
+              <strong>Capital:</strong>
+              {country.capital}
+            </p>
           </div>
           <div>
-            <p><strong>Top level Domain:</strong>{country.topLevelDomain}</p>
+            <p>
+              <strong>Top level Domain:</strong>
+              {country.topLevelDomain}
+            </p>
             <p>
               <strong>Currencies:</strong>
-              {country.currencies.map (item => (
+              {country.currencies.map((item) => (
                 <span key={item.name}>{item.name}</span>
               ))}
             </p>
             <p className="languages">
               <strong>Languages:</strong>
-              {country.languages.map (item => (
+              {country.languages.map((item) => (
                 <span key={item.name}>{item.name}</span>
               ))}
             </p>
           </div>
         </div>
-        <p className="borders"><strong>Border Countries:</strong></p>
-        {country.borders.map (item => (
-          <span key={item} className="border-item">{item}</span>
-        ))}
+        {country.borders && (
+          <>
+            <p className="borders">
+              <strong>Border Countries:</strong>
+            </p>
+            {country.borders.map((item) => (
+              <span key={item} className="border-item">
+                {item}
+              </span>
+            ))}
+          </>
+        )}
       </div>
-
     </CountrySelectedStyled>
   );
 }
